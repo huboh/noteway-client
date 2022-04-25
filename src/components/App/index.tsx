8862812e7c8db102c6a9684c7b479a820c42b69c
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-25 20:29:23 +0100

    🥂 REFACTOR  : add `AppProviders` wrapper component & setup routing

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from "../../views/Home";
+import AppProviders from '../../providers';
+
+// views
+import Login from "../../views/Auth/Login";
+import Signup from "../../views/Auth/Signup";
+import Protected from "../../views/Protected";
 
 
 const App: FC = () => {
   return (
-    <section>
-      <Home />
-    </section>
+    <AppProviders>
+      <Routes>
+        <Route path='/' element={ <Protected /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/signup' element={ <Signup /> } />
+      </Routes>
+    </AppProviders>
   );
 };
 
