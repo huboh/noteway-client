06f1668d3f230140f04f1db3f9b43bb797e7c734
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-25 19:14:20 +0100

    🔨 REFACTOR : create component `AppProviders` to wrap all essential providers the app needs

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,1 +1,29 @@
+import { FC } from 'react';
+
+import UserProvider from './UserProvider';
+import ApolloProvider from './ApolloProvider';
 import NetworkInfoProvider from './NetworkInfoProvider';
+import BrowserRouterProvider from './BrowserRouterProvider';
+
+const combineProviders = (providers: FC[]) => providers.reduce(
+  (Providers, Provider) => function AppProviders({ children }) {
+    return (
+      <Providers>
+        <Provider>
+          { children }
+        </Provider>
+      </Providers>
+    );
+  }
+);
+
+const AppProviders = combineProviders([
+  ApolloProvider,
+  BrowserRouterProvider,
+  UserProvider,
+  NetworkInfoProvider,
+]);
+
+export {
+  AppProviders as default
+};
