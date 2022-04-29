861e5ae3b2b96caddfe7517877f179fdfd7eec50
Huboh <72440222+huboh@users.noreply.github.com>
2022-04-29 11:04:23 +0100

    😋 FEAT : integerate code splitting & suspense into app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,24 +1,27 @@
-import { FC } from "react";
+import { FC, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Loader from "../Loader";
 import AppProviders from '../../providers';
 
 import * as constants from "../../constants";
 
-// views
-import Login from "../../views/Auth/Login";
-import Signup from "../../views/Auth/Signup";
-import Protected from "../../views/Protected";
 
+// views
+const Login = lazy(() => import("../../views/Auth/Login"));
+const Signup = lazy(() => import("../../views/Auth/Signup"));
+const Protected = lazy(() => import("../../views/Protected"));
 
 const App: FC = () => {
   return (
     <AppProviders>
-      <Routes>
-        <Route path={ constants.SIGNUP_ROUTE } element={ <Signup /> } />
-        <Route path={ constants.LOGIN_ROUTE } element={ <Login /> } />
-        <Route path={ constants.HOME_ROUTE } element={ <Protected /> } />
-      </Routes>
+      <Suspense fallback={ <Loader /> }>
+        <Routes>
+          <Route path={ constants.SIGNUP_ROUTE } element={ <Signup /> } />
+          <Route path={ constants.LOGIN_ROUTE } element={ <Login /> } />
+          <Route path={ constants.HOME_ROUTE } element={ <Protected /> } />
+        </Routes>
+      </Suspense>
     </AppProviders>
   );
 };
--- src/views/Protected/index.tsx
+++ src/views/Protected/index.tsx
@@ -1,21 +1,19 @@
-import { FC } from "react";
-// import { useLocation } from 'react-router-dom';
-
+import { FC, lazy } from "react";
 import useUser from "../../hooks/useUser";
 
-import Home from "../Home";
-import Dashboard from "../Dashboard";
 
+const Home = lazy(() => import("../Home"));
+const Dashboard = lazy(() => import("../Dashboard"));
 
 const Protected: FC = () => {
-  // const { pathname } = useLocation();
   const { isLoggedIn, isLoading } = useUser();
-  // const isRequestingNote = pathname.includes('notes/');
 
   if (isLoading) return null;
   if (isLoggedIn) return <Dashboard />;
 
-  else return <Home />;
+  return (
+    <Home />
+  );
 };
 
 
