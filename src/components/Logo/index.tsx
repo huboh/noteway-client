9451897bf3c414cc5c04e76ea7da54038ffa42c0
Huboh <72440222+huboh@users.noreply.github.com>
2022-05-02 22:12:29 +0100

    🤘 CHORE : update `Logo` component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,8 @@
 import "./logo.scss";
 import { FC } from "react";
-import logo from "./logo.svg";
+import logo from "./logo-Itoma.png";
 
-import * as constants from "../../constants"
+import * as constants from "../../constants";
 
 export interface LogoProps {
   redirectLink?: string;
@@ -11,7 +11,7 @@ export interface LogoProps {
 
 const Logo: FC<LogoProps> = ({ redirectLink }) => {
   return (
-    <a href={ redirectLink || constants.HOME_ROUTE  } className="logo-wrapper">
+    <a href={ redirectLink || constants.HOME_ROUTE } className="logo-wrapper">
       <img className="logo" src={ logo } alt="" />
     </a>
   );
