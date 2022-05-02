a37b0db917423ad732647e6b3fac19c5d0ada108
Huboh <72440222+huboh@users.noreply.github.com>
2022-05-02 22:14:49 +0100

    🤙 CHORE : add forgot-password route to `constants` module

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export const TOKEN_KEY = 'token';
 
 // routes
+export const FORGOT_PASSWORD_ROUTE = "/forgot-password";
 export const LOGIN_ROUTE = "/login";
 export const SIGNUP_ROUTE = "/signup";
 export const HOME_ROUTE = "/";
