c39b6211a43eba6b035cdccdef601ac1c113678b
Huboh <72440222+huboh@users.noreply.github.com>
2022-05-03 17:24:05 +0100

    👨‍🔬 CHORE : add `SignupForm` component to `Signup` view

--- src/views/Auth/Signup/index.tsx
+++ src/views/Auth/Signup/index.tsx
@@ -1,10 +1,15 @@
 import { FC } from "react";
 
+import './signup-view.scss';
+import SignupForm from './components/SignupForm';
+
 
 const Signup: FC = () => {
   return (
-    <main>
-      Signup
+    <main className='view auth-view signup-view'>
+      <section className='form-wrapper'>
+        <SignupForm />
+      </section>
     </main>
   );
 };
