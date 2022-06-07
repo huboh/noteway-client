5ef5bc1be041196c2a399297de659996804fa943
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-07 17:48:56 +0100

    😏CHORE: create new sub module `routes` under `constant` parent module

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,1 @@
 export const TOKEN_KEY = 'token';
-
-// routes
-export const FORGOT_PASSWORD_ROUTE = "/forgot-password";
-export const DASHBOARD_ROUTE = "/dashboard";
-export const LOGIN_ROUTE = "/login";
-export const SIGNUP_ROUTE = "/signup";
-export const HOME_ROUTE = "/";
---
+++ src/constants/routes/index.ts
@@ -0,0 +1,12 @@
+export const FORGOT_PASSWORD = "/forgot-password";
+export const DASHBOARD = "/dashboard";
+export const LOGIN = "/login";
+export const SIGNUP = "/signup";
+export const HOME = "/";
+
+export const ARCHIVED_NOTES = "archived-notes";
+export const SETTINGS = "settings";
+export const PROFILE = "profile";
+export const NOTES = "notes";
+export const TRASH = "trash";
+export const TAGS = "tags";
