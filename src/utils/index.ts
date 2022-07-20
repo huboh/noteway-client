86ff49692d240d2364dd8af307a30e5b41aefaf9
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-20 23:06:39 +0100

    create util function to properly join  classname string

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export const isPromise = <T = any>(value: T): value is T => (
   Boolean(value && typeof (value as any).then === "function")
 );
+
+export const joinClassStrings = (...classNames: (string | undefined)[]) => (
+  classNames.reduce((a, b) => `${(a || "").trim()} ${(b || "").trim()}`.trim(), "")
+);
