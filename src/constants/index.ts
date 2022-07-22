a93ca138b7598d1c53e8cc21e002ab9fabd9e22e
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-22 23:23:48 +0100

    😬 CHORE: add new constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,8 @@
+import { Theme } from "../types";
+import { BiLoaderCircle } from 'react-icons/bi';
+
 export const TOKEN_KEY = 'token';
+
+export const Spinner = BiLoaderCircle;
+
+export const Themes: Theme[] = ['light', 'dark', 'system'];
