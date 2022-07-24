1d415f28cdcdfa781dc04e9a27721b14739cfbfe
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-24 19:57:58 +0100

    🤙 REFACTOR: add `ErrorBoundary` & `Router` component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,31 +1,21 @@
-import { FC, lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { FC, Suspense } from "react";
 
 import Loader from "../Loader";
+import Router from "../Router";
 import AppProviders from '../../providers';
-
-import * as constants from "../../constants";
-
-
-// views
-const Login = lazy(() => import("../../views/Auth/Login"));
-const Signup = lazy(() => import("../../views/Auth/Signup"));
-const Protected = lazy(() => import("../../views/Protected"));
+import ErrorBoundary from "../ErrorBoundary";
 
 const App: FC = () => {
   return (
-    <AppProviders>
-      <Suspense fallback={ <Loader /> }>
-        <Routes>
-          <Route path={ constants.SIGNUP_ROUTE } element={ <Signup /> } />
-          <Route path={ constants.LOGIN_ROUTE } element={ <Login /> } />
-          <Route path={ constants.HOME_ROUTE } element={ <Protected /> } />
-        </Routes>
-      </Suspense>
-    </AppProviders>
+    <ErrorBoundary>
+      <AppProviders>
+        <Suspense fallback={ <Loader /> }>
+          <Router />
+        </Suspense>
+      </AppProviders>
+    </ErrorBoundary>
   );
 };
-
 
 export {
   App as default
