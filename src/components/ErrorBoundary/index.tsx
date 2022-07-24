02afc3be9002f53019b16207be58b1350b82309f
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-24 19:56:05 +0100

    😬 CHORE: create `ErrorBoundary` component

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,29 @@
+import { FC, Component } from 'react';
+import ErrorScreen from '../ErrorScreen';
+
+export interface ErrorBoundaryProps {
+  Fallback?: FC<{ error: Error; }>;
+}
+
+export default class ErrorBoundary extends Component<ErrorBoundaryProps> {
+  state = {
+    error: null
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return {
+      error
+    };
+  }
+
+  public render() {
+    const { error } = this.state;
+    const { children, Fallback } = this.props;
+
+    if (error) return (
+      Fallback ? <Fallback error={ error } /> : <ErrorScreen error={ error } />
+    );
+
+    return children;
+  }
+}
