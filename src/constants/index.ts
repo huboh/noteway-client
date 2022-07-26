5c0d99fa5dd7590a2c3bfaec8d623e162052108c
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-26 20:21:24 +0100

    😬 CHORE: add new constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,3 +6,8 @@ export const TOKEN_KEY = 'token';
 export const Spinner = BiLoaderCircle;
 
 export const Themes: Theme[] = ['light', 'dark', 'system'];
+
+
+// note
+export const DefaultNoteTitleText = "-- no title --";
+export const DefaultNoteContextText = "-- no content --";
