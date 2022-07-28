848971e6db467fdca45926cbb5bcd4ddf213dbe6
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-28 20:11:21 +0100

    😬 CHORE: add `LayoutIconMap` constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
-import { Theme } from "../types";
+import { FC } from "react";
+import { Theme, Layout } from "../types";
 import { BiLoaderCircle } from 'react-icons/bi';
+import { TbLayoutGrid, TbLayoutList } from "react-icons/tb";
 
 export const TOKEN_KEY = 'token';
 
@@ -7,6 +9,10 @@ export const Spinner = BiLoaderCircle;
 
 export const Themes: Theme[] = ['light', 'dark', 'system'];
 
+export const LayoutIconMap: Record<Layout, FC> = {
+  list: TbLayoutList,
+  grid: TbLayoutGrid,
+};
 
 // note
 export const DefaultNoteTitleText = "-- no title --";
