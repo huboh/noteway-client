08db7180e27bab1e3ec178ee14cbe19b82186fb1
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-29 21:27:11 +0100

    🤙 REFACTOR: add implementation for `useNotes`

--- src/hooks/useNotes/index.ts
+++ src/hooks/useNotes/index.ts
@@ -1,1 +1,16 @@
-export { }
+import { NOTES } from "../../graphql/query";
+import { useQuery } from "@apollo/client";
+import { Note, Connection } from "../../types";
+
+interface UseNotesProps {
+  authorId: string;
+}
+
+const useNotes = (props: UseNotesProps) => useQuery<{ notes: Connection<Note>; }>(NOTES, {
+  fetchPolicy: "cache-and-network",
+  variables: { authorId: props.authorId },
+});
+
+export {
+  useNotes as default
+};
