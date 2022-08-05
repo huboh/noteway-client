ff8bf8af48158a6d6f2e81c419fb287d7397f4e3
Huboh <72440222+huboh@users.noreply.github.com>
2022-08-05 23:34:55 +0100

    🤙 REFACTOR: update `ErrorScreen` component

--- src/components/ErrorScreen/index.tsx
+++ src/components/ErrorScreen/index.tsx
@@ -1,19 +1,49 @@
 import "./styles/error-screen.scss";
 
-import { FC } from "react";
-import { joinClassStrings } from "../../utils";
+import { FC, ReactElement } from "react";
+import { TbInfoCircle, TbRefresh } from "react-icons/tb";
+
+import Button from "../Button";
+import useClassStrings from "../../hooks/useClassStrings";
 
 interface ErrorScreenProps {
-  error: Error;
+  error?: Error;
+  title?: string;
+  message?: string;
   className?: string;
+
+  ctaText?: string;
+  ctaIcon?: ReactElement;
+  ctaFunction?: () => void;
 }
 
+const DefaultErrorTitle = "Sorry, there's been a problem";
+const DefaultErrorMessage = "Click the button below to reload the page. We might be having some technical issues if this keep happening contact us";
+
 const ErrorScreen: FC<ErrorScreenProps> = (props) => {
-  const classString = joinClassStrings("error-screen-wrapper", props.className);
+  const btnText = props.ctaText || "reload page";
+  const btnIcon = props.ctaIcon || <TbRefresh />;
+  const title = props.title || DefaultErrorTitle;
+  const message = props.message || props.error?.message || DefaultErrorMessage;
+
+  const classString = useClassStrings("error-screen-wrapper", props.className);
 
   return (
     <div className={ classString }>
-      <p className="error-message">{ props.error.message }</p>
+      <TbInfoCircle
+        className={ "error-icon" }
+      />
+
+      <div className="error-messages">
+        <h1 className={ "error-title" } children={ title } />
+        <p className={ "error-message" } children={ message } />
+      </div>
+
+      <Button.Primary
+        text={ btnText }
+        icon={ btnIcon }
+        onClick={ () => props.ctaFunction ? props.ctaFunction() : window.location.reload() }
+      />
     </div>
   );
 };
