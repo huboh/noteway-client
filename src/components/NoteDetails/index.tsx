57c437760aecad8875089856aea92098190e71bf
Huboh <72440222+huboh@users.noreply.github.com>
2022-08-09 21:53:17 +0100

    🤙 REFACTOR: add skeleton loading for `NoteDetails` component

--- src/components/NoteDetails/index.tsx
+++ src/components/NoteDetails/index.tsx
@@ -1,7 +1,7 @@
 import "./styles/note-details.scss";
 
-import { Note } from "../../types";
 import { FC } from "react";
+import { Note } from "../../types";
 import { formatDate } from "../../utils";
 
 // hooks
@@ -14,26 +14,26 @@ import NoteDetail from "./components/NoteDetail";
 import NoteDetailsSkeleton from "./components/NoteDetailsSkeleton";
 
 export interface NoteDetailsProps {
-  note: Note;
+  note?: Note;
   isLoading: boolean;
   className?: string;
 }
 
 const NoteDetails: FC<NoteDetailsProps> = (props) => {
-  const { user } = useAuth();
-  const { isAuthor } = useUserRole({ user, note: props.note });
+  const auth = useAuth();
+  const userRole = useUserRole({ user: auth.user, note: props.note });
   const classString = useClassStrings("note-details", props.className);
 
-  if (props.isLoading) return (
-    <NoteDetailsSkeleton className={ props.className } />
-  );
+  if (props.isLoading) {
+    return <NoteDetailsSkeleton className={ props.className } />;
+  };
 
   return (
     <ul className={ classString }>
-      <NoteDetail title="visibility" value={ props.note.visibility } />
-      <NoteDetail title="author" value={ isAuthor ? "You" : props.note.author.username } />
-      <NoteDetail title="modified" value={ formatDate(new Date(props.note.updatedAt)) } />
-      <NoteDetail title="created" value={ formatDate(new Date(props.note.createdAt)) } />
+      <NoteDetail title="visibility" value={ props?.note?.visibility } />
+      <NoteDetail title="author" value={ userRole.isAuthor ? "You" : props?.note?.author?.username || "" } />
+      <NoteDetail title="modified" value={ formatDate(new Date(props?.note?.updatedAt || Date.now())) } />
+      <NoteDetail title="created" value={ formatDate(new Date(props?.note?.createdAt || Date.now())) } />
     </ul>
   );
 };
