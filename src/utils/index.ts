1fad18667b4b46cbd6c2b5ec58a1e63bbef690c9
Huboh <72440222+huboh@users.noreply.github.com>
2022-08-22 17:04:28 +0100

    😬 CHORE: create util function to format date

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,11 @@ export const isPromise = <T = any>(value: T): value is T => (
 export const joinClassStrings = (...classNames: (string | undefined)[]) => (
   classNames.reduce((a, b) => `${(a || "").trim()} ${(b || "").trim()}`.trim(), "")
 );
+
+export const formatDate = (date: Date) => {
+  const isAm = date.getHours() < 12;
+  const hours12 = (date.getHours() % 12) || 12;
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${hours12}:${minutes} ${isAm ? "AM" : "PM"}, ${date.toDateString()}`;
+};
